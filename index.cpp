dbe74c1f49c3acbd9a9fde7a52d4e87343b8a890
xinhuawu0501 <b06106035@ntu.edu.tw>
2024-09-05 11:19:19 +0800

    feat: detect win

--- index.cpp
+++ index.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <vector>
+#include <utility> 
+
 using namespace std;
 
 //constants
@@ -11,7 +14,7 @@ const char PLAYER_MAP[3] = {' ','O','X'};
 
 //function prototype
 void printBoard(int board[ROW][COL]);
-
+int detectWin(int board[ROW][COL]);
 
 int main(){
     int board[ROW][COL] = {0};
@@ -28,24 +31,34 @@ int main(){
     }while(input == -1 || (input != PLAYER1 && input != PLAYER2));
 
     currentPlayer = input;
-
     printf("Game start!! Player %d enter movement \n", currentPlayer);
 
+
     while(moveCounter < MAX_MOVE){
-        int i = 0, j = 0;
+        int i, j;
+
+        //TODO: validate i & j
         printf("Player %d enter [row] [col] you want to move to: ", currentPlayer);
         scanf("%d %d", &i, &j);
 
         board[i][j] = currentPlayer;
-        moveCounter++;
-
+        
         printf("Player %c choose row %d col %d \n", PLAYER_MAP[currentPlayer], i, j);
         printBoard(board);
 
+        int winner = detectWin(board);
+        if(winner!=0) {
+            printf("Winner is %d!!\n", winner);
+            return 0;
+        }
+
+        //switch player & update movement counter
         currentPlayer = currentPlayer == PLAYER1 ? PLAYER2 : PLAYER1;
+        moveCounter++;
+
     }
 
-    printf("Board filled!!");
+    printf("Tie!!\n");
 
      
     return 0;
@@ -67,4 +80,39 @@ void printBoard(int board[ROW][COL]){
 
         }
     }
+}
+
+int detectWin(int board[ROW][COL]){
+    //check row
+    for(int i = 0; i < ROW; i++){
+        int start = board[i][0];
+        bool isSame = true;
+        for(int j = 0; j < COL; j++){
+            if(board[i][j] != start){
+                isSame = false;
+                break;
+            }
+        }
+        if(isSame) return start;
+    }
+
+    //check col
+    for(int j = 0; j < COL; j++){
+        int start = board[0][j];
+        bool isSame = true;
+        for(int i = 0; i < ROW; i++){
+            if(board[i][j] != start) {
+                isSame = false;
+                break;
+            }
+        }
+
+        if(isSame) return start;
+    }
+
+    //check diagonals
+    if(board[0][0] == board[1][1] && board[1][1] == board[2][2]) return board[0][0];
+    if(board[0][2] == board[1][1] && board[1][1] == board[2][0]) return board[0][2];
+
+    return 0;
 }
