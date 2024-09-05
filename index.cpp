c702d810351b9132f599d7c9a778a3a0d5c9dd96
xinhuawu0501 <b06106035@ntu.edu.tw>
2024-09-05 09:54:23 +0800

    feat: create basic game logic

--- index.cpp
+++ index.cpp
@@ -1,17 +1,21 @@
 #include <iostream>
 using namespace std;
 
-
+//constants
 const int ROW = 3;
 const int COL = 3;
+const int PLAYER1 = 1;
+const int PLAYER2 = 2;
+const int MAX_MOVE = ROW * COL;
+const char PLAYER_MAP[3] = {' ','O','X'};
 
+//function prototype
 void printBoard(int board[ROW][COL]);
 
 
 int main(){
     int board[ROW][COL] = {0};
-    const int PLAYER1 = 1;
-    const int PLAYER2 = 2;
+    int moveCounter = 0;
 
     int input = -1;
     int currentPlayer;
@@ -19,11 +23,29 @@ int main(){
     printBoard(board);
 
     do{
-        printf("Please enter first player:");
+        printf("Please enter first player (%d/%d):", PLAYER1, PLAYER2);
         scanf("%d", &input);
     }while(input == -1 || (input != PLAYER1 && input != PLAYER2));
 
     currentPlayer = input;
+
+    printf("Game start!! Player %d enter movement \n", currentPlayer);
+
+    while(moveCounter < MAX_MOVE){
+        int i = 0, j = 0;
+        printf("Player %d enter [row] [col] you want to move to: ", currentPlayer);
+        scanf("%d %d", &i, &j);
+
+        board[i][j] = currentPlayer;
+        moveCounter++;
+
+        printf("Player %c choose row %d col %d \n", PLAYER_MAP[currentPlayer], i, j);
+        printBoard(board);
+
+        currentPlayer = currentPlayer == PLAYER1 ? PLAYER2 : PLAYER1;
+    }
+
+    printf("Board filled!!");
 
      
     return 0;
@@ -32,7 +54,7 @@ int main(){
 void printBoard(int board[ROW][COL]){
     for(int row = 0; row < ROW; row++){
         for(int col = 0; col < COL; col++){
-        char c = (board[row][col] == 0) ? ' ' : (board[row][col] == 1) ? 'O' : 'X';
+        char c = PLAYER_MAP[board[row][col]];
             if(col < COL - 1) {
                 printf(" %c |", c);
             }
